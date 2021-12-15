9649ed00f80352d6598a26a0a64f53db78191557
Julien Rousseau <julrousseau20@gmail.com>
2021-12-14 22:11:06 -0500

    Config rollup auto execute server

--- server/rollup.config.js
+++ server/rollup.config.js
@@ -1,4 +1,31 @@
 import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+
+const production = !process.env.ROLLUP_WATCH;
+
+function serve() {
+	let server;
+
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+	return {
+		writeBundle() {
+			if (server) return;
+			server = require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
+				stdio: ["ignore", "inherit", "inherit"],
+				shell: true,
+			});
+
+			process.on("SIGTERM", toExit);
+			process.on("exit", toExit);
+		},
+	};
+}
 
 export default {
 	input: "src/main.ts",
@@ -6,5 +33,12 @@ export default {
 		dir: "dist",
 		format: "cjs",
 	},
-	plugins: [typescript()],
+	onwarn: (warning, warn) => {
+		if (warning.code === "EVAL") return;
+		warn(warning);
+	},
+	plugins: [json(), commonjs(), nodeResolve(), typescript(), !production && serve(), production && terser()],
+	watch: {
+		clearScreen: false,
+	},
 };
