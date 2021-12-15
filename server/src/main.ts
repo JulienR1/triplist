71bab784a4625e8c83a061393a3d200b9c6d08bb
Julien Rousseau <julrousseau20@gmail.com>
2021-12-14 22:12:00 -0500

    Express init

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,1 +1,10 @@
-console.log("hello world!");
+import express, { Request, Response } from "express";
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+	return res.sendStatus(200);
+});
+
+app.listen(port, () => console.log(`Listening on ${port}`));
