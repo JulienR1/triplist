74d0b32ea314c4ada610687fe21ddfe5c8ff0a35
Julien Rousseau <julrousseau20@gmail.com>
2021-12-17 10:54:47 -0500

    Simple app promise error handler

---
+++ app/src/components/UnknownError.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let onRetry: () => void | undefined = undefined;
+</script>
+
+<div class="unknown-error">
+	<figure>
+		<img src="./assets/error_icon.svg" alt="error symbol" />
+	</figure>
+	<p>Une erreur est survenue</p>
+	{#if onRetry}
+		<button on:click={onRetry}>Réessayer</button>
+	{/if}
+</div>
+
+<style lang="scss">
+	.unknown-error {
+		@import "../styles/style.scss";
+		@include sticky-control;
+	}
+</style>
