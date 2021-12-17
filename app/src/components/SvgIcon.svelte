09647d2169f6867f2bedb1a41e55df0fb2e4c444
Julien Rousseau <julrousseau20@gmail.com>
2021-12-17 15:27:31 -0500

    svg icon component

---
+++ app/src/components/SvgIcon.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { afterUpdate } from "svelte";
+
+	export let src: string;
+	export let size = 20;
+	export let color: string = undefined;
+
+	let svgElement;
+
+	afterUpdate(() => {
+		updateSvgStyle();
+	});
+
+	const updateSvgStyle = () => {
+		if (svgElement) {
+			const svgDocument: Document = svgElement.contentDocument;
+			const svg = svgDocument.querySelector("svg");
+
+			svg.width.baseVal.value = size;
+			svg.height.baseVal.value = size;
+			svg.style.fill = color;
+		}
+	};
+</script>
+
+<object title="svg icon" data={src} type="image/svg+xml" on:load={updateSvgStyle} bind:this={svgElement} />
+
+<style lang="scss">
+</style>
