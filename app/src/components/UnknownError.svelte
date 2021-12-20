d22c950696db948a0e7151cd981a5ce57eaf146e
Julien Rousseau <julrousseau20@gmail.com>
2021-12-20 11:29:22 -0500

    Updated error message styling

--- app/src/components/UnknownError.svelte
+++ app/src/components/UnknownError.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+	import { SvgSource } from "./svg/SvgSource";
+
+	import SvgIcon from "./SvgIcon.svelte";
+
 	export let onRetry: () => void | undefined = undefined;
 </script>
 
 <div class="unknown-error">
 	<figure>
-		<img src="./assets/error_icon.svg" alt="error symbol" />
+		<SvgIcon src={SvgSource.ERROR} color={"var(--light-red)"} size={30} />
 	</figure>
 	<p>Une erreur est survenue</p>
 	{#if onRetry}
@@ -16,5 +20,43 @@
 	.unknown-error {
 		@import "../styles/style.scss";
 		@include sticky-control;
+
+		figure {
+			width: fit-content;
+			margin: 0 auto;
+		}
+
+		button {
+			border: none;
+			outline: none;
+			cursor: pointer;
+			padding: 3px 6px;
+			color: var(--dark-blue);
+			background-color: transparent;
+			position: relative;
+
+			&::after {
+				content: "";
+				left: 50%;
+				width: 60%;
+				bottom: -1px;
+				display: block;
+				position: absolute;
+				transform: translateX(-50%);
+				border: 1px solid var(--dark-blue);
+
+				max-width: 0;
+				visibility: hidden;
+				transition: all 0.2s ease;
+			}
+
+			&:hover,
+			&:focus {
+				&::after {
+					visibility: visible;
+					max-width: 100%;
+				}
+			}
+		}
 	}
 </style>
--- app/src/components/svg/SvgSource.ts
+++ app/src/components/svg/SvgSource.ts
@@ -1,4 +1,5 @@
 export enum SvgSource {
 	CHECK = "/assets/check.svg",
 	CANCEL = "/assets/cancel.svg",
+	ERROR = "/assets/error_icon.svg",
 }
