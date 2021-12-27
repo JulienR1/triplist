dfc4093db7359dc723ed4dfbb769a7f475c77061
Julien Rousseau <julrousseau20@gmail.com>
2021-12-27 03:11:57 -0500

    server deploy process

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cp -r .git .git2
+rm -rf .git
+
+git init
+git add .
+git reset .github/*
+git reset app/*
+git reset database/*
+git commit -m "automatic publish"
+git push https://git.heroku.com/trip-luggage.git master --force
+
+rm -rf .git
+cp -r .git2 .git
