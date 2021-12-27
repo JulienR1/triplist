75b18ba80675498d29b47c168b30a377e07640cf
Julien Rousseau <julrousseau20@gmail.com>
2021-12-27 12:14:39 -0500

    Fixed server build process

--- server/rollup.config.js
+++ server/rollup.config.js
@@ -1,33 +1,27 @@
-import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 import json from "@rollup/plugin-json";
 import run from "@rollup/plugin-run";
 import excludeDependencies from "rollup-plugin-exclude-dependencies-from-bundle";
 
 const production = !process.env.ROLLUP_WATCH;
+const externals = ["dotenv", "express", "mysql2", "cors", "common/"];
 
 export default {
-	input: "src/main.ts",
-	output: {
-		dir: "dist",
-		format: "cjs",
-	},
-	onwarn: (warning, warn) => {
-		if (warning.code === "EVAL") return;
-		warn(warning);
-	},
-	plugins: [
-		excludeDependencies(),
-		json(),
-		commonjs(),
-		nodeResolve(),
-		typescript(),
-		!production && run(),
-		production && terser(),
-	],
-	watch: {
-		clearScreen: false,
-	},
+    input: "src/main.ts",
+    output: {
+        dir: "dist",
+        format: "cjs",
+    },
+    onwarn: (warning, warn) => {
+        if (warning.code === "UNRESOLVED_IMPORT" && externals.find((external) => warning.source.includes(external))) {
+            return;
+        }
+        // if (warning.code === "EVAL") return;
+        warn(warning);
+    },
+    plugins: [excludeDependencies(), json(), typescript(), !production && run(), production && terser()],
+    watch: {
+        clearScreen: false,
+    },
 };
