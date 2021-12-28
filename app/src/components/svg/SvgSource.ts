2c28cf38a364aa134bbd3e062a8fd2224ed186a0
Julien Rousseau <julrousseau20@gmail.com>
2021-12-27 19:01:06 -0500

    Incremented version

--- app/src/components/svg/SvgSource.ts
+++ app/src/components/svg/SvgSource.ts
@@ -1,6 +1,6 @@
 export enum SvgSource {
-	CHECK = "/assets/check.svg",
-	CANCEL = "/assets/cancel.svg",
-	ERROR = "/assets/error_icon.svg",
-	DELETE = "/assets/delete.svg",
+	CHECK = "./assets/check.svg",
+	CANCEL = "./assets/cancel.svg",
+	ERROR = "./assets/error_icon.svg",
+	DELETE = "./assets/delete.svg",
 }
