d65bddf27acf7fc99c4b375652616c5d236d3ab5
Julien Rousseau <julrousseau20@gmail.com>
2022-01-02 15:46:13 -0500

    Changed filtering logic

--- server/src/resources/CategoryResource.ts
+++ server/src/resources/CategoryResource.ts
@@ -5,43 +5,51 @@ import { DatabaseHandler } from "../persistance/databasehandler";
 import { getItemsForCategory } from "./ItemResource";
 
 const getCategories = async (filters: string[]): Promise<ICategory[]> => {
-    const query = `SELECT * FROM category ${filters.length > 0 ? `WHERE label IN (${Array(filters.length).fill("?").join(",")})` : ""}`;
-    const [rows] = (await DatabaseHandler.execute(query, filters.length > 0 ? filters : undefined)) as RowDataPacket[];
+	const baseQuery = "SELECT * FROM category";
+	const filteredQuery = `SELECT DISTINCT c.* FROM category c JOIN checked_items ci ON c.id = ci.category_id AND checked = TRUE JOIN activity a ON a.id = ci.activity_id AND a.label IN (${Array(
+		filters.length
+	)
+		.fill("?")
+		.join(",")})`;
 
-    const itemPromises: Promise<IItem>[] = rows.map((category: ICategory) => getItemsForCategory(filters, category));
-    const items = await Promise.all(itemPromises);
-    return rows.map((category: ICategory, index: number) => ({ ...category, items: items[index] }));
+	const query = filters.length > 0 ? filteredQuery : baseQuery;
+	const params = filters.length > 0 ? filters : undefined;
+	const [rows] = (await DatabaseHandler.execute(query, params)) as RowDataPacket[];
+
+	const itemPromises: Promise<IItem>[] = rows.map((category: ICategory) => getItemsForCategory(filters, category));
+	const items = await Promise.all(itemPromises);
+	return rows.map((category: ICategory, index: number) => ({ ...category, items: items[index] }));
 };
 
 const updateCategory = async ({ id, label, items }: ICategory): Promise<ICategory | undefined> => {
-    let updatedCategoryData: ICategory | undefined = undefined;
-    await DatabaseHandler.useConnection(async (connection) => {
-        await connection.execute("UPDATE category SET label = ? WHERE id = ?", [label, id]);
-        const [rows] = (await connection.execute("SELECT * FROM category WHERE id = ?", [id])) as RowDataPacket[];
-        if (rows.length === 1) {
-            updatedCategoryData = { ...rows[0], items };
-        }
-    });
+	let updatedCategoryData: ICategory | undefined = undefined;
+	await DatabaseHandler.useConnection(async (connection) => {
+		await connection.execute("UPDATE category SET label = ? WHERE id = ?", [label, id]);
+		const [rows] = (await connection.execute("SELECT * FROM category WHERE id = ?", [id])) as RowDataPacket[];
+		if (rows.length === 1) {
+			updatedCategoryData = { ...rows[0], items };
+		}
+	});
 
-    return updatedCategoryData;
+	return updatedCategoryData;
 };
 
 const addCategory = async ({ label }: ICategory): Promise<ICategory | undefined> => {
-    let newCategoryData: ICategory | undefined = undefined;
-    await DatabaseHandler.useConnection(async (connection) => {
-        await connection.execute("INSERT INTO category (label) VALUES (?)", [label]);
-        const [rows] = (await connection.execute("SELECT * FROM category WHERE label = ?", [label])) as RowDataPacket[];
-        // TODO: What happens if there are 2 categories w/ the same name?
-        if (rows.length === 1) {
-            newCategoryData = rows[0];
-        }
-    });
+	let newCategoryData: ICategory | undefined = undefined;
+	await DatabaseHandler.useConnection(async (connection) => {
+		await connection.execute("INSERT INTO category (label) VALUES (?)", [label]);
+		const [rows] = (await connection.execute("SELECT * FROM category WHERE label = ?", [label])) as RowDataPacket[];
+		// TODO: What happens if there are 2 categories w/ the same name?
+		if (rows.length === 1) {
+			newCategoryData = rows[0];
+		}
+	});
 
-    return newCategoryData;
+	return newCategoryData;
 };
 
 const removeCategory = async ({ id }: ICategory): Promise<void> => {
-    await DatabaseHandler.execute("DELETE FROM category WHERE id = ?", [id]);
+	await DatabaseHandler.execute("DELETE FROM category WHERE id = ?", [id]);
 };
 
 export { getCategories, addCategory, removeCategory, updateCategory };
--- server/src/resources/ItemResource.ts
+++ server/src/resources/ItemResource.ts
@@ -6,99 +6,147 @@ import { RowDataPacket } from "mysql2";
 import { DatabaseHandler } from "./../persistance/databasehandler";
 
 const getItemsForCategory = async (activityFilters: string[], { id }: ICategory): Promise<IItem[] | undefined> => {
-    if (positiveIntegerIsValid(id)) {
-        let items: IItem[] | undefined = undefined;
+	if (positiveIntegerIsValid(id)) {
+		let items: IItem[] | undefined = undefined;
 
-        await DatabaseHandler.useConnection(async (connection) => {
-            const checksParams = activityFilters.length > 0 ? [id, ...activityFilters] : [id];
-            const checksQuery = `SELECT * FROM checked_items WHERE category_id = ? ${
-                activityFilters.length > 0 ? `AND activity_id IN (SELECT id FROM activity WHERE label IN (${Array(activityFilters.length).fill("?").join(",")}))` : ""
-            }`;
+		await DatabaseHandler.useConnection(async (connection) => {
+			const hasFilters = activityFilters.length > 0;
 
-            const itemsPromise = connection.execute("SELECT * FROM item WHERE category_id = ?", [id]);
-            const checksPromise = connection.execute(checksQuery, checksParams);
-            const [[rawItems], [rawChecks]] = await Promise.all([itemsPromise, checksPromise]);
+			const [rawActivityIds] = (await connection.execute(
+				`SELECT id FROM activity ${
+					hasFilters ? `WHERE label IN (${Array(activityFilters.length).fill("?").join(",")})` : ""
+				}`,
+				hasFilters ? activityFilters : undefined
+			)) as RowDataPacket[];
+			const activityIds = rawActivityIds.map((rawId: { id: number }) => rawId.id);
 
-            if (rawItems && rawChecks) {
-                const storedItems = rawItems as IItem[];
-                const checks = rawChecks as ICheckedItem[];
-                items = storedItems.map((item: IItem) => ({ ...item, values: checks.filter((checkData) => checkData.item_id === item.id).map(({ checked }) => new Boolean(checked).valueOf()) }));
-            }
-        });
+			const checksParams = hasFilters ? [id, ...activityIds] : [id];
+			const checksQuery = `SELECT * FROM checked_items WHERE category_id = ? ${
+				hasFilters ? `AND checked = TRUE AND activity_id IN (${Array(activityIds.length).fill("?").join(",")})` : ""
+			}`;
 
-        return items;
-    }
-    throw new Error(`Invalid category id: '${id}'`);
+			const itemsPromise = connection.execute(
+				`SELECT DISTINCT i.* FROM item i ${
+					hasFilters
+						? `JOIN checked_items ci ON i.id = ci.item_id AND checked = TRUE AND activity_id IN (${Array(
+								activityIds.length
+						  )
+								.fill("?")
+								.join(",")})`
+						: ""
+				} WHERE i.category_id = ?`,
+				hasFilters ? [...activityIds, id] : [id]
+			);
+			const checksPromise = connection.execute(checksQuery, checksParams);
+			const [[rawItems], [rawChecks]] = await Promise.all([itemsPromise, checksPromise]);
+
+			if (rawItems && rawChecks) {
+				const storedItems = rawItems as IItem[];
+				const checks = rawChecks as ICheckedItem[];
+				items = storedItems.map((item: IItem) => ({
+					...item,
+					values: activityIds.map(
+						(activityId: number) =>
+							new Boolean(
+								checks.find((check) => check.activity_id === activityId && check.item_id === item.id)?.checked || false
+							)
+					),
+				}));
+			}
+		});
+
+		return items;
+	}
+	throw new Error(`Invalid category id: '${id}'`);
 };
 
 const addItemToCategory = async (itemName: string, { id }: ICategory): Promise<IItem | undefined> => {
-    if (!positiveIntegerIsValid(id)) {
-        throw new Error(`Invalid category id: '${id}'`);
-    }
+	if (!positiveIntegerIsValid(id)) {
+		throw new Error(`Invalid category id: '${id}'`);
+	}
 
-    let storedItemData: IItem | undefined = undefined;
-    await DatabaseHandler.useConnection(async (connection) => {
-        await connection.execute("INSERT INTO item (label, category_id) VALUES (?, ?)", [itemName, id]);
-        const [rows] = (await connection.execute("SELECT * FROM item WHERE label = ? AND category_id = ?", [itemName, id])) as RowDataPacket[];
+	let storedItemData: IItem | undefined = undefined;
+	await DatabaseHandler.useConnection(async (connection) => {
+		await connection.execute("INSERT INTO item (label, category_id) VALUES (?, ?)", [itemName, id]);
+		const [rows] = (await connection.execute("SELECT * FROM item WHERE label = ? AND category_id = ?", [
+			itemName,
+			id,
+		])) as RowDataPacket[];
 
-        if (rows.length === 1) {
-            const itemData = rows[0] as IItem;
-            const [activityChecks] = (await connection.execute("SELECT * FROM checked_items WHERE item_id = ? AND category_id = ?", [itemData.id, id])) as RowDataPacket[];
-            storedItemData = { ...itemData, values: Array(activityChecks.length).fill(false) };
-        }
-    });
+		if (rows.length === 1) {
+			const itemData = rows[0] as IItem;
+			const [activityChecks] = (await connection.execute(
+				"SELECT * FROM checked_items WHERE item_id = ? AND category_id = ?",
+				[itemData.id, id]
+			)) as RowDataPacket[];
+			storedItemData = { ...itemData, values: Array(activityChecks.length).fill(false) };
+		}
+	});
 
-    return storedItemData;
+	return storedItemData;
 };
 
 const updateItem = async ({ id, label, values }: IItem): Promise<IItem | undefined> => {
-    let updatedItem: IItem | undefined = undefined;
+	let updatedItem: IItem | undefined = undefined;
 
-    await DatabaseHandler.useConnection(async (connection) => {
-        await connection.execute("UPDATE item SET label = ? WHERE id = ?", [label, id]);
-        const [rows] = (await connection.execute("SELECT * FROM item WHERE id = ?", [id])) as RowDataPacket[];
-        if (rows.length === 1) {
-            updatedItem = { ...rows[0], values };
-        }
-    });
+	await DatabaseHandler.useConnection(async (connection) => {
+		await connection.execute("UPDATE item SET label = ? WHERE id = ?", [label, id]);
+		const [rows] = (await connection.execute("SELECT * FROM item WHERE id = ?", [id])) as RowDataPacket[];
+		if (rows.length === 1) {
+			updatedItem = { ...rows[0], values };
+		}
+	});
 
-    return updatedItem;
+	return updatedItem;
 };
 
 const checkItem = async (item: IItem): Promise<boolean[]> => {
-    let storedChecks: boolean[] = [];
+	let storedChecks: boolean[] = [];
 
-    await DatabaseHandler.useConnection(async (connection) => {
-        const [checkRows] = (await connection.execute("SELECT * FROM checked_items WHERE item_id = ?", [item.id])) as RowDataPacket[];
-        const checks = (checkRows as ICheckedItem[]).map((check) => ({ ...check, checked: new Boolean(check.checked).valueOf() }));
+	await DatabaseHandler.useConnection(async (connection) => {
+		const [checkRows] = (await connection.execute("SELECT * FROM checked_items WHERE item_id = ?", [
+			item.id,
+		])) as RowDataPacket[];
+		const checks = (checkRows as ICheckedItem[]).map((check) => ({
+			...check,
+			checked: new Boolean(check.checked).valueOf(),
+		}));
 
-        if (item.values.length !== checks.length) {
-            throw new Error("Inconsistent activity count when updating the checks.");
-        }
+		if (item.values.length !== checks.length) {
+			throw new Error("Inconsistent activity count when updating the checks.");
+		}
 
-        const promises = item.values
-            .map((value, index) => {
-                if (value !== checks[index].checked) {
-                    const { item_id, activity_id } = checks[index];
-                    if (value) {
-                        return connection.execute("INSERT INTO activityitem (item_id, activity_id) VALUES (?, ?)", [item_id, activity_id]);
-                    } else {
-                        return connection.execute("DELETE FROM activityitem WHERE item_id = ? AND activity_id = ?", [item_id, activity_id]);
-                    }
-                }
-            })
-            .filter((promise) => promise !== undefined);
-        await Promise.all(promises);
+		const promises = item.values
+			.map((value, index) => {
+				if (value !== checks[index].checked) {
+					const { item_id, activity_id } = checks[index];
+					if (value) {
+						return connection.execute("INSERT INTO activityitem (item_id, activity_id) VALUES (?, ?)", [
+							item_id,
+							activity_id,
+						]);
+					} else {
+						return connection.execute("DELETE FROM activityitem WHERE item_id = ? AND activity_id = ?", [
+							item_id,
+							activity_id,
+						]);
+					}
+				}
+			})
+			.filter((promise) => promise !== undefined);
+		await Promise.all(promises);
 
-        const [rawStoredChecks] = (await connection.execute("SELECT * FROM checked_items WHERE item_id = ?", [item.id])) as RowDataPacket[];
-        storedChecks = (rawStoredChecks as ICheckedItem[]).map((check) => new Boolean(check.checked).valueOf());
-    });
+		const [rawStoredChecks] = (await connection.execute("SELECT * FROM checked_items WHERE item_id = ?", [
+			item.id,
+		])) as RowDataPacket[];
+		storedChecks = (rawStoredChecks as ICheckedItem[]).map((check) => new Boolean(check.checked).valueOf());
+	});
 
-    return storedChecks || Array(item.values.length).fill(false);
+	return storedChecks || Array(item.values.length).fill(false);
 };
 
 const deleteItem = async ({ id }: IItem): Promise<void> => {
-    await DatabaseHandler.execute("DELETE FROM item WHERE id = ?", [id]);
+	await DatabaseHandler.execute("DELETE FROM item WHERE id = ?", [id]);
 };
 
 export { getItemsForCategory, addItemToCategory, updateItem, checkItem, deleteItem };
