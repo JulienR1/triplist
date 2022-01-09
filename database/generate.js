83ff55e57b3c3bab8b2b51bc7ccaab851a59552a
Julien Rousseau <julrousseau20@gmail.com>
2022-01-09 15:47:08 -0500

    Generate version specific sql script

--- database/generate.js
+++ database/generate.js
@@ -4,9 +4,17 @@ const path = require("path");
 const fileoderJson = fs.readFileSync(path.join(__dirname, "fileorder.json"), "utf-8");
 const fileorder = JSON.parse(fileoderJson);
 
-const generatedScript = fileorder.reduce((totalScript, currentFile) => {
+const subfolder = process.argv[2];
+const trimmedFileorder = subfolder ? fileorder.filter((file) => file.split("/")[0] === subfolder) : fileorder;
+
+if (trimmedFileorder.length === 0) {
+	throw new Error(`Could not generate a script for version '${subfolder}'`);
+}
+
+const generatedScript = trimmedFileorder.reduce((totalScript, currentFile) => {
 	const currentScript = fs.readFileSync(path.join(__dirname, "scripts", `${currentFile}.sql`), "utf-8");
 	return `${totalScript}\n${currentScript}`;
 }, "");
 
-fs.writeFileSync(path.join(__dirname, "out.sql"), generatedScript, "utf-8");
+const outputFilename = `out${subfolder ? `_${subfolder}` : ""}.sql`;
+fs.writeFileSync(path.join(__dirname, outputFilename), generatedScript, "utf-8");
